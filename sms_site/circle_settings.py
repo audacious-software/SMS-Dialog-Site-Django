461a6c3384cac7a35d1d66eb2b2f7b1b8e8e7989
Chris J. Karr <chris@audacious-software.com>
2023-02-15 18:41:37 -0600

    Updating CI testing configuration

--- sms_site/circle_settings.py
+++ sms_site/circle_settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'CHANGEME' # nosec
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['example.com']
 
 DATABASES = {
     'default': {
