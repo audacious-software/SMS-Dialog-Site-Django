b65308bc6d8b80f2550d22b7f6927cef7e361075
Chris J. Karr <chris@audacious-software.com>
2025-08-21 20:52:47 +0000

    Circle CI cleanup

--- .circleci/circle_settings.py
+++ .circleci/circle_settings.py
@@ -32,7 +32,7 @@ SIMPLE_MESSAGING_TWILIO_PHONE_NUMBER = 'CHANGEME' # nosec
 SIMPLE_MESSAGING_COUNTRY_CODE = 'US'
 
 SILENCED_SYSTEM_CHECKS = [
-    'security.W004'
+    'security.W004',
     'simple_messaging.W002',
     'simple_messaging_switchboard.E001',
     'simple_messaging_switchboard.E002',
